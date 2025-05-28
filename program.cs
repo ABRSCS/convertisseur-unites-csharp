19c62302b2b453aaee30f441355e25cc510b83ae
ABRSCS <abrassecasse@gmail.com>
2025-05-28 12:22:10 +0200

    version interactive et nettoyage

--- program.cs
+++ program.cs
@@ -1,31 +1,28 @@
-using System;
+﻿using System;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("=== Convertisseur de longueurs ===");
-        Console.Write("Entrez une longueur en centimètres : ");
-        
-        string input = Console.ReadLine();
-        double cm;
-
-        if (double.TryParse(input, out cm))
+        while (true)
         {
-            double m = cm / 100;
-            double inch = cm / 2.54;
-            double feet = cm / 30.48;
+            Console.Write("Entrez une longueur en centimètres (ou 'q' pour quitter) : ");
+            string input = Console.ReadLine();
+            if (input.ToLower() == "q") break;
 
-            Console.WriteLine($"\n{cm} cm = {m} m");
-            Console.WriteLine($"{cm} cm = {inch:F2} pouces");
-            Console.WriteLine($"{cm} cm = {feet:F2} pieds");
+            if (double.TryParse(input, out double cm))
+            {
+                Console.WriteLine($"{cm} cm = {cm / 100} m");
+                Console.WriteLine($"{cm} cm = {cm * 10} mm");
+                Console.WriteLine($"{cm} cm = {cm / 2.54:F2} pouces");
+                Console.WriteLine($"{cm} cm = {cm / 30.48:F2} pieds\n");
+            }
+            else
+            {
+                Console.WriteLine("Valeur invalide. Veuillez entrer un nombre.\n");
+            }
         }
-        else
-        {
-            Console.WriteLine("Valeur invalide. Veuillez entrer un nombre.");
-        }
-
-        Console.WriteLine("\nAppuyez sur une touche pour quitter.");
-        Console.ReadKey();
+        Console.WriteLine("Au revoir !");
     }
 }
